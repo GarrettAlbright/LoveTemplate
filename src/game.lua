f0c34c0cecd0fa707aaaba02f037c7d6abcd7f9e
Garrett Albright <contact@albright.pro>
2021-09-28 16:02:11 -0600

    Add a blue gradient background. Currently has a bug where the spinning icon will also appear to have a blue hue until the "game" is paused.

--- src/game.lua
+++ src/game.lua
@@ -5,6 +5,7 @@ Game.isPaused = false
 
 function Game:load()
   self.font = love.graphics.newFont(20)
+  self:generateGradient()
   self.player:load()
 end
 
@@ -17,6 +18,7 @@ function Game:update(dt)
 end
 
 function Game:draw()
+  love.graphics.draw(self.bgGradient)
   self.player:draw()
 
   if self.isPaused then
@@ -33,6 +35,18 @@ end
 
 function Game:setPause(pause)
   self.isPaused = pause
+end
+
+function Game:generateGradient()
+  local winW, winH = love.window.getMode()
+  self.bgGradient = love.graphics.newCanvas(winW, winH)
+  love.graphics.setCanvas(self.bgGradient)
+  for yPos = 0, winH - 1 do
+    local color = ((yPos / winH) * .4) + .4
+    love.graphics.setColor(0, 0, color)
+    love.graphics.line(0, yPos, winW, yPos)
+  end
+  love.graphics.setCanvas()
 end
 
 return Game
